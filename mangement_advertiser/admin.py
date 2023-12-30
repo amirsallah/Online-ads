cea0264950919b04ad8105881a5fd29f22e43c3d
amirsallah <amir1381basketball@gmail.com>
2023-12-30 17:07:43 +0330

    add and register admin database improve models.py

--- mangement_advertiser/admin.py
+++ mangement_advertiser/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ad, Advertiser
 
-# Register your models here.
+admin.site.register(Ad)
+admin.site.register(Advertiser)
--- mangement_advertiser/models.py
+++ mangement_advertiser/models.py
@@ -1,10 +1,49 @@
+# models.py
+
 from django.db import models
 
 
-class BaseAdvertising:
+class Ad(models.Model):
+    title = models.CharField(max_length=255)
+    img_url = models.URLField(blank=True)
+    link = models.URLField()
+    unique_id_ad = models.IntegerField(primary_key=True)
+    clicks_ad = models.IntegerField(default=0)
+    views_ad = models.IntegerField(default=0)
+    advertiser = models.ForeignKey('Advertiser', on_delete=models.CASCADE, related_name='ads')
+
+    def __str__(self):
+        return self.title
+
+    def describe_me(self):
+        return "This is the Ad class. It is used to store information about ads."
+
+    def inc_clicks(self):
+        self.clicks_ad += 1
+
+    def inc_views(self):
+        self.views_ad += 1
+
+    def get_clicks(self):
+        return self.clicks_ad
+
+    def get_views(self):
+        return self.views_ad
+    def get_url(self):
+        print(self.img_url.url)
+
+
+class Advertiser(models.Model):
+    name = models.CharField(max_length=255)
     unique_id = models.IntegerField(primary_key=True)
     clicks = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def describe_me(self):
+        return "This is the Advertiser class. It is used to store information about advertisers."
 
     def inc_clicks(self):
         self.clicks += 1
@@ -17,49 +56,3 @@ class BaseAdvertising:
 
     def get_views(self):
         return self.views
-
-    def describe_me(self):
-        return "This is the BaseAdvertising class. It is used to store information about ads and advertisers."
-
-    def _check_unique_id(self):
-        pass
-
-
-class Advertiser(BaseAdvertising, models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-    def describe_me(self):
-        return "This is the Advertiser class. It is used to store information about advertisers."
-
-    @staticmethod
-    def get_total_clicks():
-        total_clicks = 0
-        advertisers = Advertiser.objects.all()
-        for advertiser in advertisers:
-            total_clicks += advertiser.get_clicks()
-        return total_clicks
-
-    @staticmethod
-    def help():
-        string = ""
-        string += "id - The id of the advertiser.\n"
-        string += "name - The name of the advertiser.\n"
-        string += "clicks - The number of clicks the advertiser has.\n"
-        string += "views - The number of views the advertiser has.\n"
-        return string
-
-
-class Ad(BaseAdvertising, models.Model):
-    title = models.CharField(max_length=255)
-    img_url = models.URLField()
-    link = models.URLField()
-    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-    def describe_me(self):
-        return "This is the Ad class. It is used to store information about ads."
