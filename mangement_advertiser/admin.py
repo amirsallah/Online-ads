9653f61df3db357f7f5d09f44fe66c3ba958a798
amirsallah <amir1381basketball@gmail.com>
2024-01-01 21:13:22 +0330

    upgrade Admin panel for search and filter

--- mangement_advertiser/admin.py
+++ mangement_advertiser/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Ad, Advertiser
+from .models import Ad, Advertiser, View, Click
 
-admin.site.register(Ad)
+
+class AdAdmin(admin.ModelAdmin):
+    list_display = ['title', 'advertiser', 'approve']
+    list_editable = ['approve']
+    list_filter = ['approve']
+    search_fields = ['title', 'advertiser__name']
+
+
+admin.site.register(View)
+admin.site.register(Click)
 admin.site.register(Advertiser)
+admin.site.register(Ad, AdAdmin)
